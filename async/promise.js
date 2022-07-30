54ae7fb4643c10f9b11ae0e7e9c0ce2ed8ce8d15
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2022-07-30 22:33:30 +0600

    promise in js

---
+++ async/promise.js
@@ -0,0 +1,32 @@
+
+console.log('Task 1');
+const status = true; 
+
+/**
+ * function define 
+ */
+const promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        if(status){
+            resolve('Task 2')
+        }
+        else{
+            reject('failed');
+        }
+    },2000)
+});
+
+/**
+ * function call 
+ */
+promise 
+    .then(function(success){
+        console.log(success)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
+console.log('Task 3');
+
+
