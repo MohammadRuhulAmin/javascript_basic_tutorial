23f1f64cc60fa6dff67cde7a646ecefcd9c453fe
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-08-03 09:12:55 +0600

    Promise intro 2

--- Promise/promise2.js
+++ Promise/promise2.js
@@ -19,40 +19,40 @@ const promise_3 = new Promise((resolve,reject)=>{
     else reject('The work is not done for promise_3')
 })
 
-// promise_1
-//     .then((resolve)=>{
-//         console.log(resolve)
-//     })
-//     .catch((reject)=>{
-//         console.log(reject)
-//     })
+promise_1
+    .then((resolve)=>{
+        console.log(resolve)
+    })
+    .catch((reject)=>{
+        console.log(reject)
+    })
 
 
-// promise_2
-//     .then((resolve)=>{
-//         console.log(resolve);
-//     })
-//     .catch((error)=>{
-//         console.log(error)
-//     })
+promise_2
+    .then((resolve)=>{
+        console.log(resolve);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 
-// promise_3 
-//     .then((resolve)=>{
-//         console.log(resolve);
-//     })
-//     .catch((error)=>{
-//         console.log(error)
-//     })
+promise_3 
+    .then((resolve)=>{
+        console.log(resolve);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 
 
-Promise.all([promise_1,promise_2,promise_3])
-       .then(([resolve_1,resolve_2,resolve_3])=>{
-            console.log(resolve_1);
-            console.log(resolve_2);
-            console.log(resolve_3);
-        })
-        .catch(([error_1,error_2,error_3])=>{
-            console.log(error_1);
-            console.log(error_2);
-            console.log(error_3);
-        })
+// Promise.all([promise_1,promise_2,promise_3])
+//        .then(([resolve_1,resolve_2,resolve_3])=>{
+//             console.log(resolve_1);
+//             console.log(resolve_2);
+//             console.log(resolve_3);
+//         })
+//         .catch(([error_1,error_2,error_3])=>{
+//             console.log(error_1);
+//             console.log(error_2);
+//             console.log(error_3);
+//         })
